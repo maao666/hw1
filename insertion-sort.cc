4b29fb02231b1ea8a42e6488ce2f2673e003e358
Aparna Chandramowlishwaran <amowli@csail.mit.edu>
2020-01-07 23:25:45 -0800

    Driver for HW1

---
+++ insertion-sort.cc
@@ -0,0 +1,19 @@
+/**
+ *  \file insertion-sort.cc
+ *
+ *  \brief Implement your insertion sort in this file.
+ */
+
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "sort.hh"
+
+void
+mySort (int N, keytype* A)
+{
+  /* Lucky you, you get to start from scratch */
+}
+
+/* eof */
